7c080a941862a270b7532d7a147a915f9088ae86
Maarten van Nimwegen <maartenvannimwegen@hotmail.com>
2025-09-25 10:43:41 +0200

    Search system added

--- Grocery.App/Views/GroceryListItemsView.xaml.cs
+++ Grocery.App/Views/GroceryListItemsView.xaml.cs
@@ -4,9 +4,17 @@ namespace Grocery.App.Views;
 
 public partial class GroceryListItemsView : ContentPage
 {
-	public GroceryListItemsView(GroceryListItemsViewModel viewModel)
-	{
-		InitializeComponent();
+    public GroceryListItemsView(GroceryListItemsViewModel viewModel)
+    {
+        InitializeComponent();
         BindingContext = viewModel;
+    }
+
+    private void OnSearchBarTextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (BindingContext is GroceryListItemsViewModel vm)
+        {
+            vm.SearchCommand.Execute(e.NewTextValue);
+        }
     }
 }
